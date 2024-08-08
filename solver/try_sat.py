ccd639cee60fb4dd9df18d1f1ef03e9750cf6d93
Vinter-mach <vintermach@gmail.com>
2024-08-08 15:24:29 +0500

    add_sudoku_solver

---
+++ solver/try_sat.py
@@ -0,0 +1,55 @@
+from ortools.sat.python import cp_model
+
+model = cp_model.CpModel()
+
+position = dict()
+
+for i in range(9):
+    for j in range(9):
+        position[f'({i}, {j})'] = model.new_int_var(1, 9, f'({i}, {j})')
+
+# check square constraints
+for delta_x in range(3):
+    for delta_y in range(3):
+        position_in_square = []
+        for x in range(3):
+            for y in range(3):
+                position_in_square.append(f'({x + 3 * delta_x}, {y + 3 * delta_y})')
+        for i in range(len(position_in_square)):
+            for j in range(i + 1, len(position_in_square)):
+                model.add(position[position_in_square[i]] != position[position_in_square[j]])
+
+# check vertical and horizontal constraints
+for i in range(9):
+    position_in_vertical = []
+    position_in_horizontal = []
+    for j in range(9):
+        position_in_vertical.append(f'({j}, {i})')
+        position_in_horizontal.append(f'({i}, {j})')
+    for k in range(len(position_in_vertical)):
+        for m in range(k + 1, len(position_in_vertical)):
+            model.add(position[position_in_vertical[k]] != position[position_in_vertical[m]])
+            model.add(position[position_in_horizontal[k]] != position[position_in_horizontal[m]])
+
+# check diagonal constraints
+for i in range(18):
+    diag_x = i
+    diag_y = 0
+    if i >= 9:
+        diag_x, diag_y = diag_y, diag_x
+    position_in_diagonal = []
+    while -1 < diag_x < 9 and -1 < diag_y < 9:
+        position_in_diagonal.append(f'({diag_x}, {diag_y})')
+        diag_x += 1
+        diag_y += 1
+    for k in range(len(position_in_diagonal)):
+        for m in range(k + 1, len(position_in_diagonal)):
+            model.add(position[position_in_diagonal[k]] != position[position_in_diagonal[m]])
+
+solver = cp_model.CpSolver()
+status = solver.solve(model)
+print(status)
+for i in range(9):
+    print()
+    for j in range(9):
+        print(solver.value(position[f'({i}, {j})']), end=" ")
